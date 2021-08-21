c55791ea154cb8c43331f9c29833406639ee854e
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-21 14:02:12 +0530

    More on tests

--- 01_revision_of_basics/13_writing_tests/main_test.go
+++ 01_revision_of_basics/13_writing_tests/main_test.go
@@ -11,10 +11,44 @@ func TestDivide(t *testing.T) {
 	}
 }
 
-// Run `go test` or `go test -v` to run the tests
+// Run `go test` or `go test -v` to run the tests. Use `go test -cover` to check coverage.
+// Also, Coverage report: go test -coverprofile=coverage.out && go tool cover -html=coverage.out
 func TestBadDivide(t *testing.T) {
 	_, err := divide(10.0, 0)
 	if err == nil {
 		t.Error("Did not get an error when we should have.") // Throw testing errors like this
+	}
+}
+
+// Better way
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isErr    bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"expect-5", 50.0, 10.0, 5.0, false},
+}
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		res, err := divide(tt.dividend, tt.divisor)
+
+		if tt.isErr {
+			if err == nil {
+				t.Error("expected an error but did not get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("did not expect an error but got one", err.Error())
+			}
+		}
+
+		if res != tt.expected {
+			t.Errorf("expected %f but got %f", tt.expected, res)
+		}
 	}
 }
