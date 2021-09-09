3b8ce4b7d0202509385c6443f7fbca4fb62f1717
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-09-09 23:49:01 +0530

    Using functions in HandleFunc

--- 02_web_app/main.go
+++ 02_web_app/main.go
@@ -5,16 +5,29 @@ import (
 	"net/http"
 )
 
+// Home is the home page handler
+func Home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, "This is the home page.")
+}
+
+// About is the about page handler
+func About(rw http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 3)
+	_, _ = fmt.Fprintf(rw, fmt.Sprintf("This is the about page and 2 + 3 is %d.", sum))
+
+}
+
+// addValues add 2 integers and return the saun.
+func addValues(x, y int) int {
+	return x + y
+}
+
+// When a function or variable name begins with a capital letter, it means it is accessible outside the given package
+// When a function or variable name begins with a capital letter, it means the func/var is private
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(rw, "Hello World!")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(fmt.Sprintf("Number of Bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	_ = http.ListenAndServe(":8080", nil) // _ ignores the error
 }
