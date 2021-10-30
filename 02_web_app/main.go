a68e50801f854337304a3709ddff20a1d52d46e2
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-10-30 18:55:14 +0530

    Error checking

--- 02_web_app/main.go
+++ 02_web_app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -24,12 +25,33 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(rw http.ResponseWriter, r *http.Request) {
+	x := 100.0
+	y := 0.0
+	f, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(rw, "Cannot divide by zero")
+		return
+	}
+	fmt.Fprintf(rw, "%f divided by %f is %f.", x, y, f)
+}
+
+func divideValues(x, y float64) (float64, error) {
+	if y == 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // When a function or variable name begins with a capital letter, it means it is accessible outside the given package
 // When a function or variable name begins with a small letter, it means the func/var is private
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
 
